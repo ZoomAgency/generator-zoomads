baf51bc4c6b6b67ab5cbfc5d2a829275f08c3a8b
vekvoid <vektrm@gmail.com>
2016-08-19 14:06:06 +0200

    Recursive promts implemented to create multiple formats.

--- app/index.js
+++ app/index.js
@@ -7,31 +7,65 @@ module.exports = generators.Base.extend({
   constructor: function() {
     generators.Base.apply(this, arguments);
 
+    this.ads = [];
     this.option('coffee');
   },
   prompting: function() {
-    var prompts = questions;
+    var self = this,
+        prompts = questions,
+        done = this.async();
 
     return this.prompt(prompts).then(function(props) {
-      this.props = props;
+      this.ads.push(props);
 
-    }.bind(this));
+    }.bind(this)).then(function() {
+      askForMoreBanners(self);
+
+    });
+
+    function askForMoreBanners(self) {
+      var needMoreAds = {
+        type: 'confirm',
+        name: 'moreAds',
+        message: '¿Necesitas crear un nuevo formato?'
+      };
+
+      return self.prompt([needMoreAds]).then(function(props) {
+        if (props.moreAds) {
+          self.prompting();
+        } else {
+          done();
+        }
+      }.bind(self));
+    }
   },
   configuring: function() {
     //
   },
   writing: function() {
-    this._copyTplWithContext(
-      this.templatePath('test.txt'),
-      this.destinationPath('test.txt'),
-      this.props
-    );
+    var self = this,
+        adsPropsArray = this.ads;
 
-    this._copyTplWithContext(
-      this.templatePath(this.props.platform),
-      this.destinationPath(),
-      this.props
-    );
+    adsPropsArray.forEach(function(adProps, index) {
+      console.log(adProps);
+      this._copyTplWithContext(
+        'test.txt',
+        'test-' + index + '.txt',
+        adProps
+      );
+    }.bind(self));
+
+    // this._copyTplWithContext(
+    //   this.templatePath('test.txt'),
+    //   this.destinationPath('test.txt'),
+    //   this.props
+    // );
+
+    // this._copyTplWithContext(
+    //   this.templatePath(this.props.platform),
+    //   this.destinationPath(),
+    //   this.props
+    // );
   },
   _copyTplWithContext: function(templatePath, destinationPath, context) {
     this.fs.copyTpl(
